9a8f7262e604691d0f5538133ef5ee126484407a
Zionme <captainsinai@gmail.com>
2023-04-02 01:00:06 -0700

    7. Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def add_tuple(tupe_a=(), tuple_b=()):
+def add_tuple(tuple_a=(), tuple_b=()):
     """Add two tuples."""
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
