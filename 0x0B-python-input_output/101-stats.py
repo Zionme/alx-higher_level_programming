f1fa51a628487d4d3b352b3d4e06165a8c7f0584
Zionme <captainsinai@gmail.com>
2023-05-02 23:41:53 +0000

    14. Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -20,39 +20,36 @@ def print_stats(size, status_codes):
 if __name__ == "__main__":
     import sys
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+status_count = {code: 0 for code in status_codes}
+total_size = 0
+line_count = 0
 
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
+try:
+    for line in sys.stdin:
+        line_count += 1
+        fields = line.split()
+        if len(fields) >= 7:
             try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
+                status = int(fields[6])
+                if status in status_codes:
+                    status_count[status] += 1
+            except ValueError:
                 pass
-
             try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
+                size = int(fields[8])
+                total_size += size
+            except (ValueError, IndexError):
                 pass
+        if line_count % 10 == 0:
+            print("File size: {}".format(total_size))
+            for code in sorted(status_codes):
+                if status_count[code] > 0:
+                    print("{}: {}".format(code, status_count[code]))
+except KeyboardInterrupt:
+    pass
 
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+print("File size: {}".format(total_size))
+for code in sorted(status_codes):
+    if status_count[code] > 0:
+        print("{}: {}".format(code, status_count[code]))
